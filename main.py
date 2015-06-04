9b568e68fa2a538725989fa334ebfacd604e566a
Brian Hines <brian@projectweekend.net>
2015-06-03 20:59:21 -0500

    Floats to strings

--- main.py
+++ main.py
@@ -9,13 +9,20 @@ def main():
     pi_bank = Table('pi_bank')
     ser = Serial(port='/dev/ttyAMA0', baudrate=9600)
     while True:
+        reading = json.loads(ser.readline())
+
+        # convert floats to strings for DynamoDB
+        for k, v in reading.iteritems():
+            reading[k] = str(v)
+
         data = {
             'id': str(uuid.uuid4()),
             'device': 'pi_01',
             'timestamp': int(time.time()),
             'reading_type': 'thpl_01',
-            'reading_data': json.loads(ser.readline())
+            'reading_data': reading
         }
+
         pi_bank.put_item(data=data)
 
 
