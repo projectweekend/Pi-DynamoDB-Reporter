c16ee1bc2c3098f420d9ec0309e03b18b425bc05
Brian Hines <brian@projectweekend.net>
2015-06-03 20:26:53 -0500

    Load from JSON

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from serial import Serial
 def main():
     ser = Serial(port='/dev/ttyAMA0', baudrate=9600)
     while True:
-        print(ser.readline())
+        sensor_reading = ser.readline()
+        data = json.loads(sensor_reading)
+        print(data)
 
 
 if __name__ == '__main__':
