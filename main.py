0ea9b14b6e4b8f9f124a2cf311c2b8d76a3a2b2a
Brian Hines <brian@projectweekend.net>
2015-06-03 20:42:12 -0500

    Building the reading data

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+import uuid
+import time
 import json
 from serial import Serial
+from boto.dynamodb2.table import Table
 
 
 def main():
+    # pi_bank = Table('pi_bank')
     ser = Serial(port='/dev/ttyAMA0', baudrate=9600)
     while True:
-        sensor_reading = ser.readline()
-        data = json.loads(sensor_reading)
+        data = {
+            'id': uuid.uuid4(),
+            'device': 'pi_01',
+            'timestamp': int(time.time()),
+            'reading_type': 'thpl_01',
+            'reading_data': json.loads(ser.readline())
+        }
         print(data)
 
 
